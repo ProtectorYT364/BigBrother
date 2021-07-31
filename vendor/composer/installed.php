79603ce6d601a3526b125ad7413c8f844b6219a6
Superomarking <71559522+Superomarking@users.noreply.github.com>
2021-07-31 18:18:53 +0300

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'bigbrotherteam/bigbrother',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'bigbrotherteam/bigbrother' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'phpseclib/phpseclib' => array(
+            'pretty_version' => '2.0.17',
+            'version' => '2.0.17.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpseclib/phpseclib',
+            'aliases' => array(),
+            'reference' => '3ca5b88d582c69178c8d01fd9d02b94e15042bb8',
+            'dev_requirement' => false,
+        ),
+    ),
+);
